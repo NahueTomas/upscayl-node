d8ebcad61146cc14f6686794816c7964f8a4ed9b
Nahuel Rodriguez <nahuetomas@outlook.com>
2024-06-07 19:40:20 -0300

    [FIX] Index

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ModelManager } from './upscaler/sub-classes/model-manager/model-manager';
+import { Upscaler } from './upscaler/upscaler';
+import { CommandUpscayl } from './upscaler/sub-classes/driver/command-upscayl/command-upscayl';
+import { Specs } from './upscaler/sub-classes/specs/specs';
+
+const specs = new Specs();
+const arch = specs.getArch();
+const platform = specs.getPlatform();
+
+const modelManager = new ModelManager();
+const commandUpscayl = new CommandUpscayl(arch, platform);
+
+export const upscaler = new Upscaler(commandUpscayl, modelManager);
